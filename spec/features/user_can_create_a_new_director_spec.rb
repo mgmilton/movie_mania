9b59e26609e87411924a3464b12ada1ca677442c
mgmilton <matt.milton@gmail.com>
2018-02-05 10:26:36 -0700

    adds create functionality for directors

--- spec/features/user_can_create_a_new_director_spec.rb
+++ spec/features/user_can_create_a_new_director_spec.rb
@@ -5,9 +5,10 @@ describe "as a user" do
     it "they can create a director" do
       visit "/directors/new"
 
-      fill_in "diector[name]", with: "Copola"
-      click_on "Create Director"
+      fill_in "director[name]", with: "Copola"
 
+      click_on "Create Director"
+      
       expect(page).to have_content("Director: Copola")
     end
   end
