59fa8eb90a391ce9482bcdb5014e04c48f6552c3
mgmilton <matt.milton@gmail.com>
2018-02-12 10:53:22 -0700

    updates routes and sessions_controller

---
+++ spec/features/user_can_log_out_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+describe "As a visitor" do
+  describe "when I visit the root" do
+    it "he or she can login" do
+      user = User.create(username: "funbucket13", password: "test")
+
+      visit '/'
+
+      click_on "I already have an account"
+
+      expect(current_path).to eq(login_path)
+
+      fill_in "username", with: user.username
+      fill_in "password", with: user.password
+
+      click_on "Log In"
+      
+      click_on "Log Out"
+
+      expect(current_path).to eq(root_path)
+    end
+  end
+end
